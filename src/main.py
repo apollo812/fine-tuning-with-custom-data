af5c8ae23a06f8f128a693753aa6ca88384bf38c
apollo812 <goodluckbusiness0101@gmail.com>
2023-12-21 20:10:18 -0500

    refact:

--- src/main.py
+++ src/main.py
@@ -12,15 +12,9 @@ project_root = os.path.abspath(os.path.join(current_script_directory, os.pardir)
 sys.path.append(project_root)
 sys.path.append(current_script_directory)
 
-from fastapi import Depends, FastAPI, File, Response, UploadFile
+from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 from starlette.status import HTTP_201_CREATED
-
-# from finetune.FineTuningClass import FineTuningClass
-# from chatting.ChattingClass import ChattingClass
-
-# from models.finetune_model import FineTuneModel
-# from models.chatting_model import ChattingModel
 
 # Create a FastAPI application
 app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
@@ -31,19 +25,3 @@ app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
 async def root():
     return RedirectResponse(app.docs_url)
 
-
-# @app.post("/finetune", status_code=HTTP_201_CREATED)
-# async def finetune(req_body: FineTuneModel):
-#     fine_tune = FineTuningClass(api_key=req_body.api_key, data_path=req_body.data_path, model=req_body.model, temperature=req_body.temperature, max_retries=req_body.max_retries)
-#     fine_tune.train_generation()
-#     fine_tune.jsonl_generation()
-#     model_id = fine_tune.finetune()
-#     return {"model_id": model_id}
-
-
-# @app.post("/chatting", status_code=HTTP_201_CREATED)
-# async def chatting(req_body: ChattingModel):
-#     chatbot = ChattingClass(model_id=req_body.model, data_path=req_body.data_path, api_key=req_body.api_key, temperature=req_body.temperature)
-#     response = chatbot.ask_question(req_body.question)
-#     print(response)
-#     return {"answer": response}
